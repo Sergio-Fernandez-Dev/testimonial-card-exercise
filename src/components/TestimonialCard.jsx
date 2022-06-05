f0e7bff67ce7feec7c65e6fc8730a2b8df51e40a
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-06-05 18:28:14 +0200

    Removing unnecessary files

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import '../stylesheets/TestimonialCard.css';
 
 function TestimonialCard(props) {
-    return (
-        <article className="testimonial-container">
-            <img
-                className="testimonial-img"
-                src={require(`../img/testimonialCard-${props.image}.png`)}
-                alt={`Foto de ${props.name}`} />
-            <section className="testimonial-text-container">
-                <p className="testimonial-name">
-                    <strong>{props.name}</strong> en {props.country}
-                </p>
-                <p className="testimonial-job-title">
-                    {props.jobTitle} en <strong>{props.company}</strong>
-                </p>
-                <p className="testimonial-text">
-                    "{props.testimony}"
-                </p>
-            </section>
-        </article>
-    );
+  return (
+    <article className="testimonial-container">
+      <img
+        className="testimonial-img"
+        src={require(`../img/testimonialCard-${props.image}.png`)}
+        alt={`Foto de ${props.name}`} />
+      <section className="testimonial-text-container">
+        <p className="testimonial-name">
+          <strong>{props.name}</strong> en {props.country}
+        </p>
+        <p className="testimonial-job-title">
+          {props.jobTitle} en <strong>{props.company}</strong>
+        </p>
+        <p className="testimonial-text">
+          "{props.testimony}"
+        </p>
+      </section>
+    </article>
+  );
 }
 
 export default TestimonialCard;
