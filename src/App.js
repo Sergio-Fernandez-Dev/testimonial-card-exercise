655512cc7de7cb059fa05e6e96c3931e48bd076f
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-06-05 18:48:02 +0200

    Adding card info throw external file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import TestimonialCard from './components/TestimonialCard';
+import cardInfo from './info/info';
 
 function App() {
   return (
@@ -7,33 +8,26 @@ function App() {
       <main className='main-container'>
         <h1>Lo que otras personas dicen de mí</h1>
         <TestimonialCard
-          name="Shawn Wang "
-          country="Singapur"
-          jobTitle="Programador Senior React"
-          company="Facebook"
-          testimony="Trabajar junto a Sergio ha sido una de las mejores experiencias que he tenido en el ámbito laboral.
-          No solo hace que lo dificil parezca sencillo, si no que además, siempre está dispuesto a echar una mano allí
-          donde haga falta."
-          image="shawn" />
+          name={cardInfo.shawn.name}
+          country={cardInfo.shawn.country}
+          jobTitle={cardInfo.shawn.jobTitle}
+          company={cardInfo.shawn.company}
+          testimony={cardInfo.shawn.testimony}
+          image={cardInfo.shawn.image} />
         <TestimonialCard 
-          name="Emma Bostian "
-          country="Suecia"
-          jobTitle="Ingeniera de Software"
-          company="Spotify"
-          testimony="Sergio es todo un profesional de la programación.
-          Ha sido un auténtico placer conocerlo y verlo trabajar.
-          A su lado he podido aprender un montón sobre código limpio y buenas prácticas.
-          Su trabajo me ha servido de inspiración y me ha ayudado a mejorar profesionalmente."
-          image="emma" />
+          name={cardInfo.emma.name}
+          country={cardInfo.emma.country}
+          jobTitle={cardInfo.emma.jobTitle}
+          company={cardInfo.emma.company}
+          testimony={cardInfo.emma.testimony}
+          image={cardInfo.emma.image} />
         <TestimonialCard 
-          name="Sarah Chima "
-          country="Estados Unidos"
-          jobTitle="Ingeniera DevOps"
-          company="Amazon"
-          testimony="En mis 18 años de carrera he tenido el placer de trabajar con algunos de los mejores
-          profesionales de sector. Tal vez la carrera de Sergio no haya hecho más que empezar, pero puedo decir,
-          sin miedo a equivocarme, que su pasión y su dedicación lo llevarán lejos."
-          image="sarah" />
+          name={cardInfo.sarah.name}
+          country={cardInfo.sarah.country}
+          jobTitle={cardInfo.sarah.jobTitle}
+          company={cardInfo.sarah.company}
+          testimony={cardInfo.sarah.testimony}
+          image={cardInfo.sarah.image} />
       </main>
     </div>
   );
---
+++ src/info/info.js
@@ -0,0 +1,35 @@
+const cardInfo = {
+  shawn: {
+    name: "Shawn Wang",
+    country: "Singapur",
+    jobTitle: "Programador Senior React",
+    company: "Facebook",
+    testimony: "Trabajar junto a Sergio ha sido una de las mejores experiencias que he tenido en el ámbito laboral.\
+      No solo hace que lo dificil parezca sencillo, si no que además, siempre está dispuesto a echar una mano allí\
+      donde haga falta.",
+    image: "shawn", 
+  },
+  emma: {
+    name: "Emma Bostian",
+    country: "Suecia",
+    jobTitle: "Ingeniera de Software",
+    company: "Spotify",
+    testimony: "Sergio es todo un profesional de la programación.\
+      Ha sido un auténtico placer conocerlo y verlo trabajar.\
+      A su lado he podido aprender un montón sobre código limpio y buenas prácticas.\
+      Su trabajo me ha servido de inspiración y me ha ayudado a mejorar profesionalmente.",
+    image: "emma",
+  },
+  sarah: {
+    name: "Sarah Chima",
+    country: "Estados Unidos",
+    jobTitle: "Ingeniera DevOps",
+    company: "Amazon",
+    testimony: "En mis 18 años de carrera he tenido el placer de trabajar con algunos de los mejores\
+      profesionales de sector. Tal vez la carrera de Sergio no haya hecho más que empezar, pero puedo decir,\
+      sin miedo a equivocarme, que su pasión y su dedicación lo llevarán lejos.",
+    image: "sarah",
+  }
+}
+
+export default cardInfo;
